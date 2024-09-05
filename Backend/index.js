a09c6fb6047bf6199fedba73b331d76fae70efc5
p-oornima-c <poornima4060242@gmail.com>
2024-09-05 20:50:44 +0530

    Express-CRUD-app

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,61 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 const port =8080
 
 app.get('/',(req,res)=> {
     res.send('Home Page')
 })
+
+let busData=[]
+let nextId=1
+
+//add buses
+app.post('/addbus',(req,res) => {
+    const {name}=req.body
+    const newBus = {id:nextId++,name}
+    busData.push(newBus)
+    res.status(200).send(newBus)
+})
+
+//All buses
+app.get('/buses',(req,res) => {
+    res.status(200).send(busData)
+})
+
+// Retrieve bus with id
+app.get('/buses/:id',(req,res) => {
+    const bus = busData.find(b => b.id === parseInt(req.params.id))
+    if(!bus){
+        return res.status(404).send('Bus not found')
+    }
+    res.status(200).send(bus)
+})
+
+
+//update bus
+app.put('/buses/:id',(req,res) => {
+    const bus =busData.find(b => b.id === parseInt(req.params.id))
+    if(!bus){
+        return res.status(404).send('Bus not found')
+    }
+    const {name}=req.body
+    bus.name =name
+    res.status(200).send(bus)
+})
+
+//delete bus
+app.delete('/buses/:id',(req,res) => {
+    const index = busData.findIndex(b => b.id === parseInt(req.params.id))
+    if (index === -1){
+        return res.status(404).send('Bus not found')
+    }
+    busData.splice(index,1)
+    return res.status(200).send(busData)
+})
+
+
 
 app.listen(port, ()=> {
     console.log(`Server running at port : ${port}.. `)
